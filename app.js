13830b54920937eeb8cb8628a0badc648e4e617f
NazishKiran <nazishkirancosmos123@gmail.com>
2025-04-27 00:53:56 +0500

    added cors

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ import connectDb from "./db/db.js";
 connectDb();
 import userRoutes from "./routes/user.routes.js";
 
-
-
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://hackathon-frontend-kohl.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -22,6 +20,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users", userRoutes);
-
 
 export default app;
