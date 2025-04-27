289a00d9d61b82b985f6027d3a106e3232667efb
NazishKiran <nazishkirancosmos123@gmail.com>
2025-04-27 11:39:46 +0500

    added task model

---
+++ models/task.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  user: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['To Do', 'In Progress', 'Done'],
+    default: 'To Do',
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.Task || mongoose.model('Task', taskSchema);
