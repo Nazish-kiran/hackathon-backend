1b512e6756ac5dc37478e96b221aae0b69bb82c4
NazishKiran <nazishkirancosmos123@gmail.com>
2025-04-27 14:52:38 +0500

    edited

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
